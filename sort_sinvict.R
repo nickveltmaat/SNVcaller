04aa813c8b7c0bab3176b95b7a5886b44d50feae
Nick Veltmaat <80262318+nickveltmaat@users.noreply.github.com>
2021-11-16 11:04:23 +0100

    Add files via upload

---
+++ sort_sinvict.R
@@ -0,0 +1,15 @@
+# Sorting .sinvict file by Chr and Pos
+
+df <- read.table(file = './temp/output-sinvict/calls_level1.sinvict', sep = '\t', header = F)
+
+#Adding 'chr'
+#df$V1 <- sub("^", "chr", df$V1 )
+
+#Order
+chrOrder <- c(paste("chr",1:22,sep=""),"chrX","chrY","chrM")
+df$V1 <- factor(df$V1, levels=chrOrder)
+df <- df[order(df$V1, df$V2),]
+
+
+#df$V1 <- sub("chr", "", df$V1 )
+write.table(df,'./temp/output-sinvict/calls_level1_sorted.sinvict',sep="\t",row.names=FALSE, col.names = FALSE, quote = FALSE)
