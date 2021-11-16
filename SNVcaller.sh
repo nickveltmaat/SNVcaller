8eda799e919378c4c9183397f86d7f4495cc8828
Nick Veltmaat <80262318+nickveltmaat@users.noreply.github.com>
2021-11-16 15:16:42 +0100

    Update SNVcaller.sh
    V3

--- SNVcaller.sh
+++ SNVcaller.sh
@@ -12,61 +12,71 @@ while getopts "R:L:I:O:" arg; do
   esac
 done
 
-# Creating temp dirs 
-mkdir /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/temp
-mkdir /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/temp/LF && mkdir /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/temp/M2 && mkdir SNVcallingPipeline/temp/SV && mkdir /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/temp/VD
-mkdir /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/temp/output-readcount && mkdir /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/temp/output-sinvict
+# Creating temp dirs
+mkdir ./temp && mkdir ./temp/M2 && mkdir ./temp/VD && mkdir ./temp/LF && mkdir ./temp/SV
+mkdir ./temp/output-readcount && mkdir ./temp/output-sinvict
 
-#TODO
-#############
-## Preprocessing .bam and .bed (removing 'chr') 
-#samtools view -H $Inputbam | sed 's/chr//g' > /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/temp/header.sam
-#samtools reheader /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/temp/header.sam $Inputbam > /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/temp/newbam.bam
-#samtools index /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/temp/newbam.bam
-#sed 's/chr//g' $Listregions > /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipeline/temp/newbed.bed
-#############
+echo -e '\nPre-process bam and bed file: \n\n'
+## Preprocessing .bam and .bed (removing 'chr')
+samtools view -H $Inputbam | sed 's/xxxxx//g' > ./temp/header.sam
+samtools reheader ./temp/header.sam $Inputbam > ./temp/newbam.bam
+samtools index ./temp/newbam.bam
+sed 's/xxxxx//g' $Listregions > ./temp/newbed.bed
 
-
-
-### Running 4 tools simultaneously
-VarDictJava/build/libs/VarDict-1.8.2.jar -G $Reference -b $Inputbam -c 1 -S 2 -E 3 $Listregions > SNVcallingPipeline/temp/VD/vardict_raw.vcf &
-sinvict/bam-readcount/build/bin/bam-readcount -l $Listregions -w 1 -f $Reference $Inputbam > SNVcallingPipeline/temp/output-readcount/output.readcount &
-gatk/gatk Mutect2 -R $Reference -I $Inputbam -O SNVcallingPipeline/temp/M2/M2Unfiltered.vcf --native-pair-hmm-threads 8 -L $Listregions &
-lofreq_star-2.1.5_linux-x86-64/bin/lofreq call-parallel --pp-threads 8 --call-indels -f $Reference -o SNVcallingPipeline/temp/LF/output_lofreq_bed.vcf -l $Listregions $Inputbam &
+############# TOOLS: 
+## Running all 4 tools simultaneously
+#Lofreq
+./tools/lofreq/src/lofreq/lofreq call-parallel --pp-threads 8 --call-indels --no-default-filter -f $Reference -o ./temp/LF/output_lofreq_bed.vcf -l ./temp/newbed.bed ./temp/newbam.bam && \
+./tools/lofreq/src/lofreq/lofreq filter -i ./temp/LF/output_lofreq_bed.vcf -o ./temp/LF/output_lofreq.vcf -v 100 -a 0.01 &
+#Mutect2
+gatk Mutect2 --verbosity ERROR --callable-depth 100 --minimum-allele-fraction 0.01 -R $Reference -I ./temp/newbam.bam -O ./temp/M2/M2Unfiltered.vcf --native-pair-hmm-threads 8 -L ./temp/newbed.bed && \
+gatk FilterMutectCalls --verbosity ERROR -R $Reference -V ./temp/M2/M2Unfiltered.vcf -O ./temp/M2/M2filtered.vcf &
+#VarDict
+~/.conda/pkgs/vardict-java-1.8.2-hdfd78af_3/bin/vardict-java -th 8 -G $Reference -b ./temp/newbam.bam -c 1 -S 2 -E 3 ./temp/newbed.bed > ./temp/VD/vardict_raw.vcf &
+#SiNVICT
+./tools/sinvict/bam-readcount/build/bin/bam-readcount -l ./temp/newbed.bed -w 1 -f $Reference ./temp/newbam.bam > ./temp/output-readcount/output.readcount && ./tools/sinvict/sinvict -m 100 -f 0.01 -t ./temp/output-readcount/ -o ./temp/output-sinvict/ &
 wait
 
+## Processing LoFreq:
+echo -e '\nProcessing LoFreq data: \n\n'
+./tools/vt/vt decompose -s -o ./temp/LF/output_lofreq-decomposed.vcf ./temp/LF/output_lofreq.vcf
+./tools/vt/vt normalize -q -n -m -o ./temp/LF/output_lofreq-decomposed-normalized.vcf -r ../hg19upper.fna ./temp/LF/output_lofreq-decomposed.vcf
+bgzip ./temp/LF/output_lofreq-decomposed-normalized.vcf
+bcftools index ./temp/LF/output_lofreq-decomposed-normalized.vcf.gz
 
-### SINVICT
-echo -e '\nRunning SiNVICT: \n\n'
-sinvict/sinvict -t SNVcallingPipeline/temp/output-readcount/ -o SNVcallingPipeline/temp/output-sinvict/
-Rscript SNVcallingPipeline/sort_sinvict.R
-python SNVcallingPipeline/sinvict_to_vcf.py SNVcallingPipeline/temp/output-sinvict/calls_level1_sorted.sinvict SNVcallingPipeline/temp/SV/sinvict-vcf-summary.vcf
-bgzip SNVcallingPipeline/temp/SV/sinvict-vcf-summary.vcf 
-bcftools index SNVcallingPipeline/temp/SV/sinvict-vcf-summary.vcf.gz
 
-### Processing Mutect 2
-echo -e '\nRunning Mutect2: \n\n'
-gatk/gatk FilterMutectCalls -R $Reference -V SNVcallingPipeline/temp/M2/M2Unfiltered.vcf -O SNVcallingPipeline/temp/M2/M2filtered.vcf
-bgzip SNVcallingPipeline/temp/M2/M2filtered.vcf
-bcftools index SNVcallingPipeline/temp/M2/M2filtered.vcf.gz
+## Processing Mutect2:
+echo -e '\nProcessing Mutect2: \n\n'
+./tools/vt/vt decompose -s -o ./temp/M2/M2filtered-decomposed.vcf ./temp/M2/M2filtered.vcf
+./tools/vt/vt normalize -q -n -m -o ./temp/M2/M2filtered-decomposed-normalized.vcf -r ../hg19upper.fna ./temp/M2/M2filtered-decomposed.vcf
+bgzip ./temp/M2/M2filtered-decomposed-normalized.vcf
+bcftools index ./temp/M2/M2filtered-decomposed-normalized.vcf.gz
 
-### Processing LoFreq
-echo -e '\nRunning LoFreq: \n\n'
-bgzip SNVcallingPipeline/temp/LF/output_lofreq_bed.vcf
-bcftools index SNVcallingPipeline/temp/LF/output_lofreq_bed.vcf.gz
-
-### Processing Vardict 
+## Processing VarDict
 echo -e '\nProcessing Vardict data: \n\n'
-Rscript SNVcallingPipeline/sort_vardict.R
-python SNVcallingPipeline/vardict_vcf.py
-bgzip SNVcallingPipeline/temp/VD/vardict_out.vcf
-bcftools index SNVcallingPipeline/temp/VD/vardict_out.vcf.gz
+Rscript ./sort_vardict.R
+source ./env/bin/activate
+python3 ./vardict_vcf.py
+deactivate
+./tools/vt/vt decompose -s -o ./temp/VD/vardict_out-decomposed.vcf ./temp/VD/vardict_out.vcf
+./tools/vt/vt normalize -q -n -m -o ./temp/VD/vardict_out-decomposed-normalized.vcf -r ../hg19upper.fna ./temp/VD/vardict_out-decomposed.vcf
+bgzip ./temp/VD/vardict_out-decomposed-normalized.vcf
+bcftools index ./temp/VD/vardict_out-decomposed-normalized.vcf.gz
+
+
+## Processing SiNVICT
+echo -e '\nProcessing SiNVICT data: \n\n'
+Rscript ./sort_sinvict.R
+source ./env/bin/activate
+python ./sinvict_to_vcf2.py ./temp/output-sinvict/calls_level1_sorted.sinvict ./temp/SV/sinvict-vcf-summary.vcf
+deactivate
+./tools/vt/vt decompose -s -o ./temp/SV/sinvict-vcf-summary-decomposed.vcf  ./temp/SV/sinvict-vcf-summary.vcf 
+./tools/vt/vt normalize -q -n -m -o ./temp/SV/sinvict-vcf-summary-decomposed-normalized.vcf  -r ../hg19upper.fna ./temp/SV/sinvict-vcf-summary-decomposed.vcf 
+bgzip ./temp/SV/sinvict-vcf-summary-decomposed-normalized.vcf
+bcftools index ./temp/SV/sinvict-vcf-summary-decomposed-normalized.vcf.gz
+
 
 ### Comparing Mutation call overlaps
-echo -e '\nComparing SNV Tools: \n'
-bcftools isec -p SNVcallingPipeline/output/ -O v -n +1 SNVcallingPipeline/temp/SV/sinvict-vcf-summary.vcf.gz SNVcallingPipeline/temp/M2/M2filtered.vcf.gz SNVcallingPipeline/temp/LF/output_lofreq_bed.vcf.gz SNVcallingPipeline/temp/VD/vardict_out.vcf.gz
+wait
 
-echo -e '\nDONE, GREAT SUCCES\n\n'
-
-
-# rm -rf SNVcallingPipeline/temp
+echo -e '\nData processed with all 4 tools\n\n'
