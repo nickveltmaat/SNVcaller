2b4e9b3e9263a6f77f36c61a4f5f3888b818f4df
Nick Veltmaat <80262318+nickveltmaat@users.noreply.github.com>
2021-11-17 14:42:51 +0100

    Added documentation

--- sinvict_to_vcf.py
+++ sinvict_to_vcf.py
@@ -1,9 +1,18 @@
+"""
+This module converts sorted sinvict output to standard .vcf format.
+Two arguments required to be given when calling this module:
+  #1: path to sorted sinvict file as input
+  #2: Path to location and name for newly generated .vcf file
+
+Author: Nick Veltmaat & Martijn Terpstra
+Date: 17-11-2021
+"""
+
 import sys
 import subprocess
 if len(sys.argv) != 3:
   print("Usage:\t" + sys.argv[0] + "\t<input_sinvict_file_path>\t<output_filename>")
   exit(0)
-  
   
 outfile = open(sys.argv[2], "w")
 outfile.write("##fileformat=VCFv4.3\n")
@@ -19,7 +28,6 @@ with open(sys.argv[1]) as infile:
 chroms2 = set(chroms)    
 for i in chroms2:
     outfile.write("##contig=<ID="+str(i)+">\n")
-
 
 outfile.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n")
 
@@ -48,4 +56,4 @@ with open(sys.argv[1]) as infile:
     else: 
       outfile.write(chromosome + "\t" + str(position) + "\t" + "." + "\t" + ref.upper() + "\t" + alt.upper() + "\t" + "." + "\t" + "PASS" + "\t" + "." + "\n")
       
-#original copied at 19okt2021 from: https://raw.githubusercontent.com/sfu-compbio/sinvict/master/sinvict_to_vcf.py
+# original copied and modified at 19okt2021 from: https://raw.githubusercontent.com/sfu-compbio/sinvict/master/sinvict_to_vcf.py
