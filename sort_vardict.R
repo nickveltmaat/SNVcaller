7f37f652a26bb57a312e78d98b9d94b3138a0e02
Nick Veltmaat <80262318+nickveltmaat@users.noreply.github.com>
2021-12-21 10:17:59 +0100

    Update sort_vardict.R
    updated argument parsing for more universal use

--- sort_vardict.R
+++ sort_vardict.R
@@ -1,8 +1,20 @@
+#!/usr/bin/env Rscript
+
 # This script automatically sorts Vardict output file by Chr and Pos
 # Author: Nick Veltmaat
 # Date: 17-11-2021
 
-df <- read.table(file = './temp/VD/vardict_raw.vcf', sep = '\t', header = F)
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is two arguments passed: if not, return an error
+if (length(args)!=2) {
+  stop("At least two arguments must be supplied (input file, output file).n", call.=FALSE)
+} 
+
+input = args[1]
+output = args[2]
+
+df <- read.table(file = toString(input), sep = '\t', header = F)
 
 #Adding 'chr'
 df$V2 <- sub("^", "chr", df$V2 )
@@ -16,4 +28,4 @@ df <- df[order(df$V2, df$V4, df$V5),]
 
 df$V2 <- sub("chr", "", df$V2 )
 df$V3 <- sub("chr", "", df$V3 )
-write.table(df,'./temp/VD/vardict_output_sorted.vcf',sep="\t",row.names=FALSE, col.names = FALSE, quote = FALSE)
+write.table(df, toString(output), sep="\t",row.names=FALSE, col.names = FALSE, quote = FALSE)
