86135a5ccf8797850cf3a75afb6ca58c60b76200
Nick Veltmaat <80262318+nickveltmaat@users.noreply.github.com>
2022-08-22 16:05:36 +0200

    Big update on SNVcaller
    V6 --> V7

--- SNVcaller.sh
+++ SNVcaller.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=SNVcaller_1_1
 #SBATCH --output="SNVtest.out"
 #SBATCH --error="SNVtest.err"
-#SBATCH --time=15:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mem=10gb
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=10
@@ -17,12 +17,11 @@
 # Add number of CPU argument (X ofzo)
 # StrandBias toevoegen
 
+# PoN when newly generated: Filter P/LP & on genomic location (intronic / synonymous / etc.) annotated blacklist can be used for this
+# Save list from P/LP filterd-out variants from PoN
 
-#Create_plots.py hernoemen naar merge_sites_with_raw_callerdata.py: Deze maakt merged_vcfs_PoN.txt (of merged_vcfs.txt)
-#nieuwe plotting.py script maken die als input sites.txt of filtered_variants.xlsx kan pakken. Alle plots achteraf dus
 
-
-while getopts "R:L:I:O:V:D:C:P:Q:B:" arg; do
+while getopts "R:L:I:O:V:D:C:P:Q:B:M:" arg; do
   case $arg in
     R) Reference=$OPTARG;;      # "/apps/data/1000G/phase1/human_g1k_v37_phiX.fasta"
     L) Listregions=$OPTARG;;    # "/groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/2020_covered_probe_notranslocation.bed"
@@ -34,6 +33,7 @@ while getopts "R:L:I:O:V:D:C:P:Q:B:" arg; do
     P) PoN=$OPTARG;;            # "/groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/GenomeScan_SequenceData/104103_normals/"
     Q) Qual=$OPTARG;;           # BaseQuality (18)
     B) Bias=$OPTARG;;           # Strand Bias (5-95)
+    M) MRD=$OPTARG;;            # Mimimal Mutant Read depth: 3
   esac
 done
 
@@ -41,11 +41,11 @@ cd /groups/umcg-pmb/tmp01/projects/hematopathology/Lymphoma/Nick/SNVcallingPipel
 
 echo -e "\nSettings: \n\nInput bam(s): $Inputbam \nReference: $Reference \nPanel: $Listregions\nminimum VAF: $VAF\nminimum Read Depth: $RDP\nminimum overlapping calls: $Calls\nPoN:$PoN"
 echo -e '\nLoading modules: \n'
-module load GATK/4.1.4.0-Java-8-LTS
+module load GATK/4.1.4.1-Java-8-LTS
 module load SAMtools/1.9-GCCcore-7.3.0
 module load BCFtools/1.11-GCCcore-7.3.0
 module load HTSlib/1.11-GCCcore-7.3.0
-module load Python/3.9.1-GCCcore-7.3.0-bare
+#module load Python/3.9.1-GCCcore-7.3.0-bare
 module load R/4.0.3-foss-2018b-bare
 module list
 
@@ -61,7 +61,7 @@ postprocess_vcf() {
 
 pythonscript() {
   source ./env/bin/activate
-  python3 $1 $2 $3 $4 $5
+  python3 $1 $2 $3 $4 $5 $6 $7
   deactivate
 }
 
@@ -95,7 +95,7 @@ gatk FilterMutectCalls \
   --min-allele-fraction $VAF \
   -R $Reference \
   -V $1 \
-  -O $2
+  -O $2 --QUIET
 }
 
 vardict() { # $1 = sample, $2 = output, $3 = bed
@@ -152,45 +152,49 @@ create_pon() {
     echo -e '\n\nGenerating VCF for normal control: '
     echo -e normalcontrol= $(basename $entry .bam) '\n'
     #Mutect2
-    mutect $entry ./PoN/normal_$(basename $entry .bam)_Mutect.vcf.gz ./PoN/newbed.bed "--max-mnp-distance 0" & \
-    
+    mutect $entry ./PoN/normal_$(basename $entry .bam)_Mutect_uf.vcf ./PoN/newbed.bed "--max-mnp-distance 0" && \
+    filter_mutect ./PoN/normal_$(basename $entry .bam)_Mutect_uf.vcf ./PoN/normal_$(basename $entry .bam)_Mutect.vcf && \
+    pythonscript ./hardfilter.py ${MRD} 'PoN' 'Mutect' ./PoN/normal_$(basename $entry .bam)_Mutect_F.vcf $RDP $(basename $entry .bam) && \
+    gatk IndexFeatureFile -I ./PoN/normal_$(basename $entry .bam)_Mutect_F.vcf --QUIET --verbosity ERROR & \
+    ########
+     
     #Vardict
-    vardict $entry ./PoN/normal_$(basename $entry .bam)_Vardict1.vcf ./PoN/newbed.bed & \
-
+    vardict $entry ./PoN/normal_$(basename $entry .bam)_Vardict1.vcf ./PoN/newbed.bed && \
+    Rscript ./sort_vardict.R ./PoN/normal_$(basename $entry .bam)_Vardict1.vcf ./PoN/normal_$(basename $entry .bam)_Vardict2.vcf && \
+    pythonscript ./vardict_vcf.py ./PoN/normal_$(basename $entry .bam)_Vardict2.vcf ./PoN/normal_$(basename $entry .bam)_Vardict.vcf && \
+    pythonscript ./hardfilter.py ${MRD} 'PoN' 'Vardict' ./PoN/normal_$(basename $entry .bam)_Vardict_F.vcf $RDP $(basename $entry .bam) && \
+    bgzip ./PoN/normal_$(basename $entry .bam)_Vardict_F.vcf && \
+    bcftools index ./PoN/normal_$(basename $entry .bam)_Vardict_F.vcf.gz & \
+    
     #Lofreq
     lofreq $entry ./PoN/normal_$(basename $entry .bam)_LofreqUnfiltered.vcf ./PoN/newbed.bed && \
-    #Filter Lofreq     
     ./tools/lofreq/src/lofreq/lofreq filter \
       -i ./PoN/normal_$(basename $entry .bam)_LofreqUnfiltered.vcf \
       -o ./PoN/normal_$(basename $entry .bam)_Lofreq.vcf  \
       -v $RDP \
-      -a $VAF & \
-       
+      -a $VAF && \
+    pythonscript ./hardfilter.py ${MRD} 'PoN' 'Lofreq' ./PoN/normal_$(basename $entry .bam)_Lofreq_F.vcf $RDP $(basename $entry .bam) && \
+    bgzip ./PoN/normal_$(basename $entry .bam)_Lofreq_F.vcf && \
+    bcftools index ./PoN/normal_$(basename $entry .bam)_Lofreq_F.vcf.gz & \
+    
     #SiNVICT
     mkdir ./PoN/$(basename $entry .bam)output-readcount/ && mkdir ./PoN/$(basename $entry .bam)output-sinvict/ && \
     sinvict $entry ./PoN/$(basename $entry .bam)output-sinvict/ ./PoN/newbed.bed  ./PoN/$(basename $entry .bam)output-readcount/ && \
-    rm -rf ./PoN/$(basename $entry .bam)output-readcount/ & \
+    rm -rf ./PoN/$(basename $entry .bam)output-readcount/ && \
+    Rscript ./sort_sinvict.R ./PoN/$(basename $entry .bam)output-sinvict/calls_level1.sinvict ./PoN/$(basename $entry .bam)output-sinvict/calls_level1_sorted.sinvict && \
+    pythonscript ./create_vcf.py ./PoN/$(basename $entry .bam)output-sinvict/calls_level1_sorted.sinvict ./PoN/normal_$(basename $entry .bam)_Sinvict.vcf $Reference 'sinvict' && \
+    pythonscript ./hardfilter.py ${MRD} 'PoN' 'Sinvict' ./PoN/normal_$(basename $entry .bam)_Sinvict_F.vcf $RDP $(basename $entry .bam) && \
+    bgzip ./PoN/normal_$(basename $entry .bam)_Sinvict_F.vcf && \
+    bcftools index ./PoN/normal_$(basename $entry .bam)_Sinvict_F.vcf.gz && \
     wait
-    
-    Rscript ./sort_vardict.R ./PoN/normal_$(basename $entry .bam)_Vardict1.vcf ./PoN/normal_$(basename $entry .bam)_Vardict2.vcf
-    pythonscript ./vardict_vcf.py ./PoN/normal_$(basename $entry .bam)_Vardict2.vcf ./PoN/normal_$(basename $entry .bam)_Vardict.vcf
-    Rscript ./sort_sinvict.R ./PoN/$(basename $entry .bam)output-sinvict/calls_level1.sinvict ./PoN/$(basename $entry .bam)output-sinvict/calls_level1_sorted.sinvict
-    pythonscript ./create_vcf.py ./PoN/$(basename $entry .bam)output-sinvict/calls_level1_sorted.sinvict ./PoN/normal_$(basename $entry .bam)_Sinvict.vcf $Reference 'sinvict'
-    
-    bgzip ./PoN/normal_$(basename $entry .bam)_Vardict.vcf
-    bcftools index ./PoN/normal_$(basename $entry .bam)_Vardict.vcf.gz
-    bgzip ./PoN/normal_$(basename $entry .bam)_Lofreq.vcf
-    bcftools index ./PoN/normal_$(basename $entry .bam)_Lofreq.vcf.gz   
-    bgzip ./PoN/normal_$(basename $entry .bam)_Sinvict.vcf
-    bcftools index ./PoN/normal_$(basename $entry .bam)_Sinvict.vcf.gz
   done
   
-  #Step 2 merge pon data
+  #Step 2 merge pon data per tool
   echo -e '\nMerging normal vcfs into GenomicsDB \n\n'
-  ls ./PoN/*Mutect.vcf.gz > ./PoN/M2normals.dat
-  ls ./PoN/*Vardict.vcf.gz > ./PoN/VDnormals.dat
-  ls ./PoN/*Lofreq.vcf.gz > ./PoN/LFnormals.dat
-  ls ./PoN/*Sinvict.vcf.gz > ./PoN/SVnormals.dat
+  ls ./PoN/*Mutect_F.vcf > ./PoN/M2normals.dat
+  ls ./PoN/*Vardict_F.vcf.gz > ./PoN/VDnormals.dat
+  ls ./PoN/*Lofreq_F.vcf.gz > ./PoN/LFnormals.dat
+  ls ./PoN/*Sinvict_F.vcf.gz > ./PoN/SVnormals.dat
   sed -i -e 's/^/ -V /' ./PoN/M2normals.dat
   sed -i -e 's/^/ /' ./PoN/VDnormals.dat
   sed -i -e 's/^/ /' ./PoN/LFnormals.dat
@@ -218,9 +222,13 @@ create_pon() {
   postprocess_vcf "./PoN/merged_PoN_Sinvict.vcf"
   postprocess_vcf "./PoN/merged_PoN_Mutect2.vcf"
   
-  bcftools isec -o ./PoN/BLACKLIST.txt -O v -n +1 ./PoN/merged_PoN_Sinvict-decomposed-normalized.vcf.gz ./PoN/merged_PoN_Mutect2-decomposed-normalized.vcf.gz ./PoN/merged_PoN_Lofreq-decomposed-normalized.vcf.gz ./PoN/merged_PoN_Vardict-decomposed-normalized.vcf.gz  #CHANGED(n tools)
+  bcftools isec -o ./PoN/BLACKLIST_uf.txt -O v -n +1 ./PoN/merged_PoN_Sinvict-decomposed-normalized.vcf.gz ./PoN/merged_PoN_Mutect2-decomposed-normalized.vcf.gz ./PoN/merged_PoN_Lofreq-decomposed-normalized.vcf.gz ./PoN/merged_PoN_Vardict-decomposed-normalized.vcf.gz  #CHANGED(n tools)
 
-  #annotating the PoN blacklist  
+  #annotating & Filtering the PoN blacklist  
+  echo -e '\n Annotating unfiltered PoN Blacklist...\n'
+  annotate "./PoN/BLACKLIST_uf.txt" "annotated_blacklist_uf"
+  echo -e '\nCleaning PoN: Removing P/LP variants & Synonymous, Intronic, UTR and Up-/downstream variants from PoN...\n'
+  pythonscript ./clean_pon.py "./PoN/annotated_blacklist_uf.xlsx" "./PoN/BLACKLIST.txt"
   echo -e '\n Annotating PoN Blacklist...\n'
   annotate "./PoN/BLACKLIST.txt" "annotated_blacklist"
   echo -e '\nDone! PoN is ready for use. Now on to analyzing samples...\n\n'
@@ -243,12 +251,12 @@ run_tools() {
   #Lofreq
   lofreq ./temp/newbam.bam ./temp/LF/output_lofreq_bed.vcf ./temp/newbed.bed && \
   #Filter Lofreq
-  filter_lofreq ./temp/LF/output_lofreq_bed.vcf ./temp/LF/LF.vcf & \
+  filter_lofreq ./temp/LF/output_lofreq_bed.vcf ./temp/LF/LF_1.vcf & \
       
   #Mutect2
   mutect ./temp/newbam.bam ./temp/M2/M2Unfiltered.vcf ./temp/newbed.bed && \
   #Filter Mutect2
-  filter_mutect ./temp/M2/M2Unfiltered.vcf ./temp/M2/M2.vcf & \
+  filter_mutect ./temp/M2/M2Unfiltered.vcf ./temp/M2/M2_1.vcf & \
     
   #VarDict
   vardict ./temp/newbam.bam ./temp/VD/vardict_raw.vcf ./temp/newbed.bed & \
@@ -258,23 +266,27 @@ run_tools() {
   wait
   
   ## Processing LoFreq:
-  echo -e '\n\Processing LoFreq data: \n'
+  echo -e '\nProcessing LoFreq data: \n'
+  pythonscript ./hardfilter.py ${MRD} 'sample' 'Lofreq' './temp/LF/LF.vcf' $RDP
   postprocess_vcf "./temp/LF/LF.vcf"
   
   ## Processing Mutect2:
   echo -e '\nProcessing Mutect2: \n'
+  pythonscript ./hardfilter.py ${MRD} 'sample' 'Mutect' './temp/M2/M2.vcf' $RDP
   postprocess_vcf "./temp/M2/M2.vcf"
   
   ## Processing VarDict
   echo -e '\nProcessing Vardict data: \n'
   Rscript ./sort_vardict.R ./temp/VD/vardict_raw.vcf ./temp/VD/vardict_output_sorted.vcf
-  pythonscript ./vardict_vcf.py ./temp/VD/vardict_output_sorted.vcf ./temp/VD/VD.vcf
+  pythonscript ./vardict_vcf.py ./temp/VD/vardict_output_sorted.vcf ./temp/VD/VD_1.vcf
+  pythonscript ./hardfilter.py ${MRD} 'sample' 'Vardict' './temp/VD/VD.vcf' $RDP
   postprocess_vcf "./temp/VD/VD.vcf"
   
   ## Processing SiNVICT
   echo -e '\nProcessing SiNVICT data: \n'
   Rscript ./sort_sinvict.R ./temp/output-sinvict/calls_level1.sinvict ./temp/output-sinvict/calls_level1_sorted.sinvict
-  pythonscript ./create_vcf.py ./temp/output-sinvict/calls_level1_sorted.sinvict ./temp/SV/SV.vcf $Reference 'sinvict'
+  pythonscript ./create_vcf.py ./temp/output-sinvict/calls_level1_sorted.sinvict ./temp/SV/SV_1.vcf $Reference 'sinvict'
+  pythonscript ./hardfilter.py ${MRD} 'sample' 'Sinvict' './temp/SV/SV.vcf' $RDP
   postprocess_vcf "./temp/SV/SV.vcf"
 
   wait 
@@ -299,32 +311,27 @@ process_bam() {
     ./temp/LF/LF-decomposed-normalized.vcf.gz \
     ./temp/VD/VD-decomposed-normalized.vcf.gz  
 
+  #Merging and Annotating SNV's
+  echo 'Merging variants stats & M2 & VD filtering steps...'   
+  pythonscript ./merge_variants.py ${xpref} "sites.txt" $RDP
+  echo 'annotating variants...'
+  annotate ./output/${xpref}/vcfs_merged.txt "annotated_SNVs"
+  
   #Plotting and Annotating blacklisted SNV's
   if [ -z "$PoN" ]
   then
     echo ""
   else
-    echo -e "source for PoN .bam files = $PoN \nBlacklisting..."
-    pythonscript ./pon_blacklist.py ${xpref}
-    
+    echo -e "source for PoN .bam files = $PoN \nBlacklisting..."   
     #Filtering, Plotting and Annotating SNV's 
-    echo 'Merging variants stats & M2 & VD filtering steps...'  
-    pythonscript ./merge_and_filter.py ${xpref} "sites_PoN.txt" $RDP
-    echo 'annotating variants filterd by pon...' 
-    annotate ./output/${xpref}/vcfs_merged_PoN.txt "annotated_SNVs_PoN_blacklisted"
     echo 'post-filtering variants filterd by pon...' 
-    pythonscript ./post_filtering.py ${xpref} "annotated_SNVs_PoN_blacklisted.xlsx"
+    pythonscript ./post_filtering.py ${xpref} "PoN"
     echo 'plotting variants filterd by pon...'
     pythonscript ./plotting.py ${xpref} "filtered_variants_PoN.xlsx"
   fi 
   
-  #Plotting and Annotating SNV's
-  echo 'Merging variants stats & M2 & VD filtering steps...'   
-  pythonscript ./merge_and_filter.py ${xpref} "sites.txt" $RDP
-  echo 'annotating variants...'
-  annotate ./output/${xpref}/vcfs_merged.txt "annotated_SNVs"
   echo 'post-filtering variants...'
-  pythonscript ./post_filtering.py ${xpref} "annotated_SNVs.xlsx"
+  pythonscript ./post_filtering.py ${xpref} "sample"
   echo 'plotting variants...'
   pythonscript ./plotting.py ${xpref} "filtered_variants.xlsx"
   
@@ -334,11 +341,14 @@ process_bam() {
   mkdir ./output/${xpref}/plots
   mv ./output/${xpref}/*.html ./output/${xpref}/plots
   mv ./output/${xpref}/*.png ./output/${xpref}/plots
+  mkdir ./output/${xpref}/filtering_info
+  mv ./output/${xpref}/filtering_info*.* ./output/${xpref}/filtering_info
+  mv ./output/${xpref}/removed_variants* ./output/${xpref}/filtering_info
   mkdir ./output/${xpref}/intermediate_files
   mv ./output/${xpref}/*.txt ./output/${xpref}/intermediate_files
-  
+    
   rm -rf ./temp  
-  echo -e "\nAnalysis of $xpref is complete!\n\n\n"
+  echo -e "\nAnalysis of $xpref is complete!\n"
 }
 
 
@@ -388,5 +398,5 @@ else
   exit 1
 fi
 
-echo -e '\nFinished run! \n'
+echo -e 'Finished run! \n\n\n'
 
