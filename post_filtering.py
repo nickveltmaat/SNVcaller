1125dc64fbf85f8dbb5475b97c6590288b2a2369
Nick Veltmaat <80262318+nickveltmaat@users.noreply.github.com>
2022-11-08 10:53:01 +0100

    Update post_filtering.py
    Updated filtering a bit, on Vaf% and MNVs

--- post_filtering.py
+++ post_filtering.py
@@ -1,15 +1,27 @@
 from functionsmodule import *
+from scipy.stats import binom
 
 sample = sys.argv[1]
 modus = sys.argv[2]
 path = './output/'
 
+#Load PCR error rate:
+mismatch_rate = pd.read_csv(path + sample + '/alignmentsummarymetrics.txt', skiprows=4, sep='\t', header=1)['PF_MISMATCH_RATE'][2].astype(float)
+
 #Load annotated variants
 annotated = pd.read_excel(path + sample + '/annotated_SNVs.xlsx', sheet_name='Variant', skiprows=1, engine='openpyxl')
+annotated['VAF'] = annotated['Samples'].str.split('AF:', 1).str[1].str.split('_').str[0].astype(float)
+annotated['DP'] = annotated['Samples'].str.split('DP:', 1).str[1].str.split(';').str[0].astype(int)
+annotated['MDP'] = (annotated['DP'] * annotated['VAF']).astype(float)
+
+#CALCULATE ERROR RATE!!!
+annotated['ErrorRate'] = (1 - binom.cdf(k=annotated['MDP'], n=annotated['DP'], p=mismatch_rate)).astype(float).round(6)
+#!!!!!!!!!!!!!!!!!!!!
 
 #Initiate filter counts df
 filtereddf = pd.DataFrame(columns=['filter', 'variants_remaining'])
 filtereddf.loc[len(filtereddf)] = ['start', len(annotated)]
+
 
 ###START FILTERING & recording filtered variants
 #Synonymous
@@ -50,47 +62,59 @@ filtereddf.loc[len(filtereddf)] = ['2kb downstream', len(annotated)]
 
 #PoN
 if modus == 'sample':
-    filteredvariantslist = [filteredvariants1, filteredvariants2, filteredvariants3, 
-                            filteredvariants4, filteredvariants5, filteredvariants6]
+    pass
 elif modus == 'PoN':
     annotated['mut'] = annotated['Chrom'].str[3:] + '_' + annotated['Pos'].astype(str)+'_' + annotated['Reference allele'] +'>' + annotated['Alternate allele']
     dfPON = pd.read_csv('./PoN/BLACKLIST.txt', sep='\t', header=None) #Load blacklist 
     dfPON['mut'] = dfPON[0].astype(str) + '_' + dfPON[1].astype(str)+'_' + dfPON[2] +'>' + dfPON[3]
     
-    filteredvariants7 = annotated[annotated.mut.isin(dfPON['mut'])].drop(['mut'], axis=1) .copy() #Save PoN variants in other df
+    filteredvariants7 = annotated[annotated.mut.isin(dfPON['mut'])].drop(['mut'], axis=1) .copy() #Save blacklisted variants in other df
     filteredvariants7['FilterReason'] = 'PoN'
-    annotated = annotated[~annotated.mut.isin(dfPON['mut'])].drop(['mut'], axis=1) #Filter out blacklisted mutations
+    annotated = annotated[~annotated.mut.isin(dfPON['mut'])].drop(['mut'], axis=1) #Filter out blacklisted variants
     
     filtereddf.loc[len(filtereddf)] = ['PoN', len(annotated)]
-    filteredvariantslist = [filteredvariants1, filteredvariants2, filteredvariants3, 
-                            filteredvariants4, filteredvariants5, filteredvariants6, filteredvariants7]
 else:
     print("Wrong input")
 
-filteredvariants = pd.concat(filteredvariantslist)
+#VAF < 0.5%
+filteredvariants8 = annotated[annotated['VAF'] < 0.005].copy()
+filteredvariants8['FilterReason'] = "VAF < 0.5%"
+annotated = annotated[annotated['VAF'] >= 0.005]
+filtereddf.loc[len(filtereddf)] = ['VAF < 0.5%', len(annotated)]    
+    
+#VAF < 1.0%
+filteredvariants9 = annotated[annotated['VAF'] < 0.01].copy()
+filteredvariants9['FilterReason'] = "VAF < 1%"
+annotated = annotated[annotated['VAF'] >= 0.01]
+filtereddf.loc[len(filtereddf)] = ['VAF < 1%', len(annotated)]
+
+#in SNP db > 1% AF
+filteredvariants10 = annotated.loc[(annotated['AF'].fillna(0) > 0.01) | (annotated['EUR AF'].fillna(0) > 0.01) |
+                            (annotated['Global AF'].fillna(0) > 0.01) | (annotated['Non-Fin Eur AF'].fillna(0) > 0.01)].copy()
+filteredvariants10['FilterReason'] = "In SNP db > 1%"
+annotated = annotated.loc[(annotated['AF'].fillna(0) <= 0.01) | (annotated['EUR AF'].fillna(0) <= 0.01) |
+                            (annotated['Global AF'].fillna(0) <= 0.01) | (annotated['Non-Fin Eur AF'].fillna(0) <= 0.01)] #Filter out SNPS with db VAF of > 1%
+filtereddf.loc[len(filtereddf)] = ['>1% AF in SNP DB', len(annotated)]   
+ 
+#MNVs:
+filteredvariants11 = annotated[annotated['Reference allele'].str.len() | annotated['Alternate allele'].str.len() > 5].copy()
+filteredvariants11['FilterReason'] = "MNV (> 5bp)"
+annotated = annotated[annotated['Reference allele'].str.len() | annotated['Alternate allele'].str.len() <= 5]
+filtereddf.loc[len(filtereddf)] = ["MNV (> 5bp)", len(annotated)]
+
+  
+if modus == 'sample':
+    filteredvariantslist = [filteredvariants1, filteredvariants2, filteredvariants3, 
+                            filteredvariants4, filteredvariants5, filteredvariants6,
+                            filteredvariants8, filteredvariants9, filteredvariants10, filteredvariants11]    
+elif modus == 'PoN':    
+    filteredvariantslist = [filteredvariants1, filteredvariants2, filteredvariants3, 
+                            filteredvariants4, filteredvariants5, filteredvariants6, filteredvariants7,
+                            filteredvariants8, filteredvariants9, filteredvariants10, filteredvariants11]    
+
+filteredvariants = pd.concat(filteredvariantslist)   
 
 
-##Counting Vardict unique calls (not filtered out ... yet)
-#Vardict unique
-#vd = annotated.copy()
-#vd['tools'] = vd['Samples'].str.split('VD:', 1).str[1].str.split('_').str[0]
-#vd[vd['tools'] != '0001']
-#filtereddf.loc[len(filtereddf)] = ['Vardict unique', len(vd[vd['tools'] != '0001'])]
-
-##Counting Variants lower than 0.5%  &  1%
-tempdf = annotated.copy()
-tempdf['VAF'] = tempdf['Samples'].str.split('AF:', 1).str[1].str.split('_').str[0].astype(float)
-tempdf = tempdf[tempdf['VAF'] >= 0.005]
-filtereddf.loc[len(filtereddf)] = ['VAF < 0.5%', len(tempdf)]
-tempdf = tempdf[tempdf['VAF'] >= 0.01]
-filtereddf.loc[len(filtereddf)] = ['VAF < 1%', len(tempdf)]
-
-#Counting Variants in SNP databases with AF > 1%
-tempdf = tempdf[tempdf['AF'].fillna(0) < 0.01]#Filter out SNPS with db VAF of > 1%
-tempdf = tempdf[tempdf['EUR AF'].fillna(0) < 0.01]
-tempdf = tempdf[tempdf['Global AF'].fillna(0) < 0.01]
-tempdf = tempdf[tempdf['Non-Fin Eur AF'].fillna(0) < 0.01]
-filtereddf.loc[len(filtereddf)] = ['>1% AF in SNP DB', len(tempdf)]
 
 
 ### Saving filtered variants, removed variants and filter counts files
